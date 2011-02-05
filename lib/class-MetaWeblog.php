3815d4ee45818fe9b7b846563e5b3ed309167690
Richard Ma <richard.ma.19850509@gmail.com>
2011-02-06 01:17:34 +0800

    modified:   class-MetaWeblog.php

--- lib/class-MetaWeblog.php
+++ lib/class-MetaWeblog.php
@@ -28,26 +28,98 @@ class MetaWeblog
     {
     }
 
+    /*
+     * Function: newMediaObject
+     *
+     * Params:
+     *      blogid
+     *      username
+     *      password
+     *      struct
+     *
+     * Return:
+     *      struct
+     */
     function newMediaObject()
     {
     }
 
+    /*
+     * Function: getCategories
+     *
+     * Params:
+     *      blogid
+     *      username
+     *      password
+     *
+     * Return:
+     *      struct
+     */
     function getCategories()
     {
     }
 
+    /*
+     * Function: getRecentPosts
+     *
+     * Params:
+     *      blogid
+     *      username
+     *      password
+     *      numberOfPosts
+     *
+     * Return:
+     *      array of struct
+     */
     function getRecentPosts()
     {
     }
 
+    /*
+     * Function: getPost
+     *
+     * Params:
+     *      postid
+     *      username
+     *      password
+     *
+     * Return:
+     *      struct
+     */
     function getPost()
     {
     }
 
+    /*
+     * Function: editPost
+     *
+     * Params:
+     *      postid
+     *      username
+     *      password
+     *      struct
+     *      publish
+     *
+     * Return:
+     *      true
+     */
     function editPost()
     {
     }
 
+    /*
+     * Function: newPost
+     *
+     * Params:
+     *      blogid
+     *      username
+     *      password
+     *      struct
+     *      publish
+     *
+     * Return:
+     *      string
+     */
     function newPost()
     {
     }
