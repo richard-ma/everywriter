9eb0d51b3cdc6a0236aced14e9cda6c3bd72d363
Richard Ma <richard.ma.19850509@gmail.com>
2011-02-06 00:48:42 +0800

    new file:   class-MetaWeblog.php new file:   class-Wp.php

---
+++ lib/class-MetaWeblog.php
@@ -0,0 +1,55 @@
+<?php
+
+class MetaWeblog
+{
+    $client = NULL;
+
+    function __construct()
+    {
+    }
+
+    function __destruct()
+    {
+    }
+
+    function getUsersBlogs()
+    {
+    }
+
+    function setTemplate()
+    {
+    }
+
+    function getTemplate()
+    {
+    }
+
+    function deletePost()
+    {
+    }
+
+    function newMediaObject()
+    {
+    }
+
+    function getCategories()
+    {
+    }
+
+    function getRecentPosts()
+    {
+    }
+
+    function getPost()
+    {
+    }
+
+    function editPost()
+    {
+    }
+
+    function newPost()
+    {
+    }
+
+}
---
+++ lib/class-Wp.php
@@ -0,0 +1,113 @@
+<?php
+
+class Wp
+{
+    function __construct()
+    {
+    }
+
+    function __destruct()
+    {
+    }
+
+    function getCommentStatusList()
+    {
+    }
+
+    function newComment()
+    {
+    }
+
+    function editComment()
+    {
+    }
+
+    function deleteComment()
+    {
+    }
+
+    function getComments()
+    {
+    }
+
+    function getComment()
+    {
+    }
+
+    function setOptions()
+    {
+    }
+
+    function getOptions()
+    {
+    }
+
+    function getPageTemplates()
+    {
+    }
+
+    function getPageStatusList()
+    {
+    }
+
+    function getPostStatusList()
+    {
+    }
+
+    function getCommentCount()
+    {
+    }
+
+    function uploadFile()
+    {
+    }
+
+    function suggestCategories()
+    {
+    }
+
+    function deleteCategory()
+    {
+    }
+
+    function newCategory()
+    {
+    }
+
+    function getTags()
+    {
+    }
+
+    function getCategories()
+    {
+    }
+
+    function getPageList()
+    {
+    }
+
+    function editPage()
+    {
+    }
+
+    function deletePage()
+    {
+    }
+
+    function newPage()
+    {
+    }
+
+    function getPages()
+    {
+    }
+
+    function getPage()
+    {
+    }
+
+    function getUsersBlogs()
+    {
+    }
+
+{
