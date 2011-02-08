0936e23bdedf4373194cbdcb0a894efe9b7ec6f0
Richard Ma <richard.ma.19850509@gmail.com>
2011-02-09 01:44:42 +0800

    Add ckeditor

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>everywriter</title>
+	<meta content="text/html; charset=utf-8" http-equiv="content-type"/>
+<script type="text/javascript" src="./editors/ckeditor/ckeditor.js"></script>
+</head>
+<body>
+<h1>Test</h1>
+<form action="./test/form-post-test.php" method="post">
+    <input name="title" type="text"/><br/>
+    <textarea class="ckeditor" name="description"></textarea><br/>
+    <input name="hidden" type="hidden" value="true"/><br/>
+</form>
+
+</body>
+</html>
+
