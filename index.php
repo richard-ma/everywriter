fcebcf214152dd44641c466908e76448d5d5c61a
Richard Ma <richard.ma.19850509@gmail.com>
2011-02-16 00:49:52 +0800

    modified:   index.php

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+
 	<title>everywriter</title>
 	<meta content="text/html; charset=utf-8" http-equiv="content-type"/>
-<script type="text/javascript" src="./editors/ckeditor/ckeditor.js"></script>
+    <script type="text/javascript" src="./editors/ckeditor/ckeditor.js"></script>
+    <script type="text/javascript" src="/js/jquery.js"></script>
+    <script type="text/javascript">
+    </script>
+
 </head>
 <body>
-<h1>Test</h1>
+
+<h1>Every Writer</h1>
 <form action="./test/form-post-test.php" method="post">
-    <input name="title" type="text"/><br/>
+    <label>Title </label><input name="title" type="text"/><br/>
     <textarea class="ckeditor" name="description"></textarea><br/>
-    <input name="hidden" type="hidden" value="true"/><br/>
+    <button name="submit" value="publish">Publish</button><button name="submit" value="draft">Draft</button><br/>
 </form>
 
 </body>
 </html>
-
